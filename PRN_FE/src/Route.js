3cc8eafe80c505ed63d30631963d41e44e634a8d
ManhToan16 <toanluon507@gmail.com>
2025-04-23 20:40:59 +0700

    add Profile and config Route

--- PRN_FE/src/Route.js
+++ PRN_FE/src/Route.js
@@ -3,12 +3,12 @@ import Home from "./components/Home.vue";
 import SignUp from "./components/SignUp.vue";
 import SignIn from "./components/SignIn.vue";
 // import SearchResult from "./components/SearchResult.vue";
-// import Profile from "./components/Profile.vue";
+import Profile from "./components/Profile.vue";
 // import CreateHost from "./components/CreateHost.vue";
 // import Posts from "./components/Posts.vue";
 // import Detail from "./components/Detail.vue";
-// import About from "./components/About.vue";
-// import Contact from "./components/Contact.vue";
+import About from "./components/About.vue";
+import Contact from "./components/Contact.vue";
 // import UpdateRoom from "./components/UpdateRoom.vue";
 // import Dashboard from "./components/Dashboard.vue";
 
@@ -40,11 +40,11 @@ const routes = [
 //       searchQuery: route.state?.searchQuery || "",
 //     }),
 //   },
-//   {
-//     name: "Profile",
-//     component: Profile,
-//     path: "/profile",
-//   },
+  {
+    name: "Profile",
+    component: Profile,
+    path: "/profile",
+  },
 //   {
 //     name: "CreateHost",
 //     component: CreateHost,
@@ -65,16 +65,16 @@ const routes = [
 //     component: UpdateRoom,
 //     path: "/update/:id",
 //   },
-//   {
-//     name: "About",
-//     component: About,
-//     path: "/about",
-//   },
-//   {
-//     name: "Contact",
-//     component: Contact,
-//     path: "/contact",
-//   },
+  {
+    name: "About",
+    component: About,
+    path: "/about",
+  },
+  {
+    name: "Contact",
+    component: Contact,
+    path: "/contact",
+  },
 //   {
 //     name: "Dashboard",
 //     component: Dashboard,
