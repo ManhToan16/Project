208e1910c71000c92661d275d9a630da669f634c
ManhToan16 <toanluon507@gmail.com>
2025-04-25 22:10:12 +0700

    git add Detail,SearchResult,Calendar and change API_Constanst

--- PRN_FE/src/Route.js
+++ PRN_FE/src/Route.js
@@ -3,11 +3,11 @@ import Home from "./components/Home.vue";
 import SignUp from "./components/SignUp.vue";
 import SignIn from "./components/SignIn.vue";
 import ManagePlace from "./components/ManagePlace.vue";
-// import SearchResult from "./components/SearchResult.vue";
+import SearchResult from "./components/SearchResult.vue";
 import Profile from "./components/Profile.vue";
 import CreateHost from "./components/CreateHost.vue";
 import Posts from "./components/Posts.vue";
-// import Detail from "./components/Detail.vue";
+import Detail from "./components/Detail.vue";
 import About from "./components/About.vue";
 import Contact from "./components/Contact.vue"; 
 import UpdateRoom from "./components/UpdateRoom.vue";
@@ -37,15 +37,15 @@ const routes = [
     name:"ManagePlace",
     component: ManagePlace,
     path: "/ManagePlace"
-  }
-//   {
-  //     name: "SearchResult",
-  //     component: SearchResult,
-  //     path: "/search",
-  //     props: (route) => ({
-  //       searchQuery: route.state?.searchQuery || "",
-  //     }),
-  //   },
+  },
+  {
+      name: "SearchResult",
+      component: SearchResult,
+      path: "/search",
+      props: (route) => ({
+        searchQuery: route.state?.searchQuery || "",
+      }),
+    },
   {
     name: "Profile",
     component: Profile,
@@ -56,11 +56,11 @@ const routes = [
     component: CreateHost,
     path: "/create-host",
   },
-//   {
-//     name: "Detail",
-//     component: Detail,
-//     path: "/detail/:id",
-//   },
+  {
+    name: "Detail",
+    component: Detail,
+    path: "/detail/:id",
+  },
   {
     name: "Posts",
     component: Posts,
